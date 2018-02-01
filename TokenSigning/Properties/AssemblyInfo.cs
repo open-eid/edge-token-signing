4c895848b81a666d2d48ee9f56437c5203889c01
Raul Metsma <raul@metsma.ee>
2018-02-01 15:01:21 +0200

    Filter certificates with attached hardware keys
    IB-5129
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- TokenSigning/Properties/AssemblyInfo.cs
+++ TokenSigning/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RIA")]
 [assembly: AssemblyProduct("TokenSigning")]
-[assembly: AssemblyCopyright("Copyright ©  2016-2017")]
+[assembly: AssemblyCopyright("Copyright ©  2016-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
 [assembly: ComVisible(false)]
