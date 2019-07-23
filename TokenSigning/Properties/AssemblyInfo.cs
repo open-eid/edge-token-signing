1546a93c3ba53e10fa1233b95014d497f6875314
Raul Metsma <raul@metsma.ee>
2019-07-23 12:33:17 +0300

    Increase version number and update certificate
    IB-5964
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- TokenSigning/Properties/AssemblyInfo.cs
+++ TokenSigning/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RIA")]
 [assembly: AssemblyProduct("TokenSigning")]
-[assembly: AssemblyCopyright("Copyright ©  2016-2018")]
+[assembly: AssemblyCopyright("Copyright ©  2016-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.3.0")]
-[assembly: AssemblyFileVersion("1.1.3.0")]
+[assembly: AssemblyVersion("1.1.4.0")]
+[assembly: AssemblyFileVersion("1.1.4.0")]
 [assembly: ComVisible(false)]
